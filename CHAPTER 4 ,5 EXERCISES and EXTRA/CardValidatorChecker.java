6053c8ac7743cbe930a7c0f1d43a58a0a6ee60f2
I-am-Byte <victor.olaomo@outlook.com>
2025-12-18 17:51:47 +0100

    modified

---
+++ CHAPTER 4 ,5 EXERCISES and EXTRA/CardValidatorChecker.java
@@ -0,0 +1,71 @@
+public class CardValidatorChecker {
+
+  
+    public static String cardTypeCheck(String number) {
+        if (number.startsWith("4")) {
+            return "Visa Card";
+        } else if (number.startsWith("5")) {
+            return "Master Card";
+       } else if (number.startsWith("37")) {
+            return "American Express Card";
+      } else if (number.startsWith("6")) {
+            return "Discover Card";
+        } else return null; 
+        
+    }
+
+
+    public static boolean digitLengthCheck(String number) {
+        if (number.length() < 13 || number.length() > 16) {
+            return false;
+        }
+        for (int count = 0; count < number.length(); count++) {
+            if (!Character.isDigit(number.charAt(count))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    
+    public static boolean cardValidityCheck(String number) {
+        int sum = 0;
+       
+        for (int index = number.length() - 1; index >= 0; index--) {
+            int newNumber = number.charAt(index)-'0';
+            if (index%2==0) {
+                newNumber *= 2;
+                if (newNumber > 9) {
+                    newNumber -= 9;
+                }
+            }
+            sum += newNumber;
+           
+        }
+        return (sum % 10 == 0);
+    }
+
+   
+    public static void validateCard(String number) {
+        String cardType = cardTypeCheck(number);
+        boolean cardDigits = digitLengthCheck(number);
+        boolean cardValidity = cardValidityCheck(number);
+
+        if (cardType == null || !cardDigits) {
+            System.out.println("\n**Invalid Card Number!**\n");
+        } else if (!cardValidity){
+        
+            System.out.printf("\n**Credit Card Type: %s%n", cardType);
+            System.out.printf("**Credit Card Number: %s%n", number);
+            System.out.printf("**Credit Card Digit Length: %d%n", number.length());
+            System.out.printf("**Credit Card Validity: Invalid%n%n");
+        }else{
+            System.out.printf("\n**Credit Card Type: %s%n", cardType);
+            System.out.printf("**Credit Card Number: %s%n", number);
+            System.out.printf("**Credit Card Digit Length: %d%n", number.length());
+            System.out.printf("**Credit Card Validity: Valid%n%n");
+        }
+      }
+    }
+    
+  
--- CHAPTER 4 ,5 EXERCISES and EXTRA/MileToKilometer.java
+++ CHAPTER 4 ,5 EXERCISES and EXTRA/MileToKilometer.java
@@ -4,12 +4,14 @@ public class mileToKIiometer {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-         System.out.println("Would you like to convert miles to kilometer or other: ");
         
-        if 
+        System.out.printf("%nTo convert Miles to Kilometer: Enter M %nTo convert Kilometers to miles: Enter K %n ");
+        char letter=input.next.charAt(0).toUpperCase();
        
+        if (letter=="K"){
+        System.out.print
         int mile = input.nextInt();
-      
-    }
+      }
+    } 
 }
 
